d11a11c4ca46c394b0f94331aaada5991eb00faf
aidanvanleuven <aidanvanleuven@gmail.com>
2015-04-11 15:50:09 -0700

    Fixed navigation sidebar
    Switched from jQuery to the proper 'Angular' way.

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var app = angular.module("app", ['ngRoute']);
 var controllers = {};
 
-controllers.SimpleController = function($scope){
+controllers.HeaderController = function HeaderController($scope, $location){
+	$scope.isActive = function (viewLocation) {
+		return viewLocation === $location.url();
+	};
+}
 
+controllers.SimpleController = function($scope, $location){
 	$scope.customers = 
 	[{name : 'Dave Adams' ,  city : 'Phoenix'} , 
 	{name: 'Brad Baker' , city : 'Portland'} , 
@@ -10,13 +15,13 @@ controllers.SimpleController = function($scope){
 	{name : 'Kyle Davis' , city : 'Boston'} , 
 	{name :'Robert Evans' , city : 'Boise'}]
 
-$scope.addCustomer = function () {	
-	$scope.customers.push(
-		{
-			name: $scope.newCustomer.name,
-			city: $scope.newCustomer.city
-		});
-	}
+	$scope.addCustomer = function () {	
+		$scope.customers.push(
+			{
+				name: $scope.newCustomer.name,
+				city: $scope.newCustomer.city
+			});
+		}
 }
 
 controllers.NavController = function($scope){
@@ -26,35 +31,35 @@ controllers.NavController = function($scope){
 			$scope.calculate.From = location.coords.latitude + "," + location.coords.longitude;
 			$scope.$apply();
 		}
-}
+	}
 	
-$scope.calculate = function (){	
-	var geocoder = new google.maps.Geocoder();
-	var process = new google.maps.DistanceMatrixService();
-	var origin;
-	var destination;
-	geocoder.geocode({ 'address' : $scope.calculate.From}, 
-	function(results, status){
-		if (status == google.maps.GeocoderStatus.OK) {
-			origin = results[0].geometry.location.k + "," + results[0].geometry.location.D;
-		} else if (status == "ZERO_RESULTS") {
-			$scope.distance = "No results found, please try again.";
-			$scope.$apply();
-		} else {
-			alert("There was an error!" + " " + status);
-		}
-	geocoder.geocode ( { 'address' : $scope.calculate.To}, function (results, status){
-		if (status == google.maps.GeocoderStatus.OK) {
-			destination = results[0].geometry.location.k + "," + results[0].geometry.location.D;
-			getDistance();		
-		} else if (status == "ZERO_RESULTS") {
-			$scope.distance = "No results found, please try again.";
-			$scope.$apply();	
-		} else {
-			alert("There was an error!" + " " + status);
-		}
-			});
+	$scope.calculate = function (){	
+		var geocoder = new google.maps.Geocoder();
+		var process = new google.maps.DistanceMatrixService();
+		var origin;
+		var destination;
+		geocoder.geocode({ 'address' : $scope.calculate.From}, function(results, status){
+			if (status == google.maps.GeocoderStatus.OK) {
+				origin = results[0].geometry.location.k + "," + results[0].geometry.location.D;
+			} else if (status == "ZERO_RESULTS") {
+				$scope.distance = "No results found, please try again.";
+				$scope.$apply();
+			} else {
+				alert("There was an error!" + " " + status);
+			}
+		geocoder.geocode ( { 'address' : $scope.calculate.To}, function (results, status){
+			if (status == google.maps.GeocoderStatus.OK) {
+				destination = results[0].geometry.location.k + "," + results[0].geometry.location.D;
+				getDistance();		
+			} else if (status == "ZERO_RESULTS") {
+				$scope.distance = "No results found, please try again.";
+				$scope.$apply();	
+			} else {
+				alert("There was an error!" + " " + status);
+			}
+		});
 	});
+	
 	function getDistance(){
 		process.getDistanceMatrix({
 			origins: [origin],
@@ -62,10 +67,12 @@ $scope.calculate = function (){
 			travelMode: google.maps.TravelMode.DRIVING,
 			unitSystem: google.maps.UnitSystem.IMPERIAL,
 		}, callback);
+		
 	function callback(response, status){
 		if (status == google.maps.DistanceMatrixStatus.OK){
 			$scope.distance = response.rows[0].elements[0].distance.text;
 			$scope.duration = response.rows[0].elements[0].duration.text;
+			console.log(response.rows[0]);
 			$scope.$apply();
 		} else {
 			alert("There was an error!" + " " + status);
